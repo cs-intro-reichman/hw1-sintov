8d6412b4459ba2af0f7af44fb55192f1458d1623
Or Sintov <sintov@Ors-Laptop.local>
2024-11-07 19:28:38 +0200

    first try of exercise submittion

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,14 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
+		System.out.println("hi");
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		int bill = Integer.parseInt(args[3]);
+		double payment_per_person = Math.ceil((double)bill/3); 
+		System.out.println("Dear " + name1 + ", " + name2 + ", " + 
+							name3 + ": the total payment per person is " + payment_per_person);
 	}
 }
