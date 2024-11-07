4461ba8e0e1e590c6ab15f9f75fd70ee05f397ab
Or Sintov <sintov@Ors-Laptop.local>
2024-11-07 19:52:49 +0200

    fourth try of exercise submittion

--- Bill3.java
+++ Bill3.java
@@ -7,7 +7,7 @@ public class Bill3 {
 		String name3 = args[2];
 		int bill = Integer.parseInt(args[3]);
 		double payment_per_person = Math.ceil((double)bill/3); 
-		System.out.println("Dear " + name1 + ", " + name2 + ", and " + 
-							name3 + ": pay " + payment_per_person + " Shekels each");
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + 
+							name1 + ": pay " + payment_per_person + " Shekels each");
 	}
 }
